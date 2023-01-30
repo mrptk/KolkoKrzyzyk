119e4e381e7dc05d9cd59466eed4135dc31b6879
mrptk <ptk.mazur@gmail.com>
2023-01-30 19:45:29 +0100

    Mechanics ready

--- src/assets/GameBoard.java
+++ src/assets/GameBoard.java
@@ -13,7 +13,7 @@ class GameBoard extends JPanel {
     private final GameFrame game;
     public boolean crossTurn;
 
-    public int unitSideDiameter = 15;
+    public int unitSideDiameter = 3;
 
     GameBoard(GameFrame game) {
         this.game = game;
